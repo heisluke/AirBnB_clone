00fbe56c2c6480975926a3d0ff502b2606bfa553
Luke Bamtefa <41142438+heisluke@users.noreply.github.com>
2023-12-11 04:12:50 +0000

    pycodestyle

---
+++ test.py
@@ -0,0 +1,18 @@
+# Filename: example.py
+
+def add_numbers(a, b):
+    # This function adds two numbers
+    result = a + b
+    return result
+
+
+def main():
+    # Main function to demonstrate code structure
+    num1 = 10
+    num2 = 20
+    sum_result = add_numbers(num1, num2)
+    print(f"The sum of {num1} and {num2} is: {sum_result}")
+
+
+if __name__ == "__main__":
+    main()
